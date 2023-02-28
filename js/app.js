c5416993aee5fa0e01471c552025c1fc16a3ae18
mohsin6862 <mohsinctg2206@gmail.com>
2023-03-01 04:09:27 +0600

    Show all catagory

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('I am alive')
+const loadCatagory = ()=>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => showCatagories(data.data.news_category))
+}
+
+const showCatagories = (catagories) =>{
+
+    console.log(catagories)
+
+    catagories.forEach(catagory => {
+
+        console.log(catagory)
+        const allCatagories = document.getElementById('display-catagory');
+        allCatagories.innerHTML += 
+        `<li><a class="text-xl font-semibold" href="#">${catagory.category_name}</a></li>`;
+        
+    });
+}
+loadCatagory()
